4be799ec5878b1d6367450c2065589923d91ae7c
thisisdoanh <100980663+thisisdoanh@users.noreply.github.com>
2023-12-24 15:14:13 +0700

    dsad

--- Code BTL Nhom2/app/src/main/java/com/example/btl_nhom2/MainActivity.java
+++ Code BTL Nhom2/app/src/main/java/com/example/btl_nhom2/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.btl_nhom2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream
+=======
+
+
+
+        HomeFragment homeFrag = new HomeFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_main, homeFrag);
+        fragmentTransaction.commit();
+
+
+
+
+>>>>>>> Stashed changes
     }
 }
